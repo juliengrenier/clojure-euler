f7cdb3b2e6ba8c986b9728a1e1ebe1eb0cba5aa6
Julien Grenier <julien.grenier42@gmail.com>
2010-09-21 04:23:52 -0400

    fix a typo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-euleur "1.0.0-SNAPSHOT"
+(defproject clojure-euler "1.0.0-SNAPSHOT"
             :description "A Clojure solutions to Project Euler"
             :dependencies [[org.clojure/clojure "1.2.0"]
                            [org.clojure/clojure-contrib "1.2.0"]
